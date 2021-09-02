b66869fd6632b9a940113bb0cedcba088128b5a7
raphaelmorsch <raphael@intersystem.eti.br>
2021-09-02 16:01:07 +0200

    First Commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/gin-hello-world", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world!",
+		})
+	})
+	router.Run(":8180")
+
+}
