95f6c6a2f82905c4919b89d42fe00954fc89444c
raphaelmorsch <raphael@intersystem.eti.br>
2021-09-02 16:45:29 +0200

    adding XML request / response support

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
-	router.GET("/gin-hello-world", func(c *gin.Context) {
+	router.GET("/gin-hello-world-json", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world!",
 		})
 	})
+	router.GET("/gin-hello-world-xml", func(c *gin.Context) {
+		c.XML(200, Message{
+			MessageText: "Hello World!",
+		})
+	})
+
 	router.Run(":8180")
 
+}
+
+type Message struct {
+	XMLName     xml.Name `xml:"message"`
+	MessageText string   `xml:"messageText,attr"`
 }
